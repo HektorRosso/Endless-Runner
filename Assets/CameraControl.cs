4e78b3e370fd95ba658be5d34b2a55888db95397
HektorRosso <iross2024@elam.co.uk>
2024-09-25 12:44:38 +0100

    Added new game elements

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float moveSpeed = 7.5f;
 
     // Create a public reference to the player - we will assign this using the Unity editor
     public GameObject player;
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();   // Get the Rigidbody2D component attached to the player
+
+    }
 
     // Update is called once per frame
     void Update()
     {
         // Change our position relation to the players position
-        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
     }
 }
